aaf2ee7b929b587b915337d0f6f2313c68ddf17c
Andréas Livet <andreas.livet@gmail.com>
2014-12-16 18:02:45 +0100

    Fix truncated results

---
+++ db/migrations/015_fix_userxp_truncate_results.rb
@@ -0,0 +1,17 @@
+Sequel.migration do
+  change do
+    DB[:user_xp].each do |xp|
+      begin
+        JSON.parse(xp.results)
+      rescue => e
+        #If there is an error, it means that the results are truncate
+        ind = xp[:results].rindex(",{")
+        if !ind.nil?
+          puts "Fix result id #{xp[:id]}"
+          xp[:results] = xp[:results][0, ind]
+          from(:user_xp).filter(id: xp[:id]).update(results: xp[:results])
+        end
+      end
+    end
+  end
+end
