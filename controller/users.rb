50cb8c8339fc514dbe80508c86b4875c2f819232
Andréas Livet <andreas.livet@stormshield.eu>
2017-02-23 23:09:20 +0100

    Remove beta warning and whitelist

--- controller/users.rb
+++ controller/users.rb
@@ -25,21 +25,15 @@ class Users < Controller
   def signup
     if request.post? and !logged_in?
       if request.params['password'] == request.params['password_confirm']
-        email = WhiteList[email: request.params['email']]
-        if email.nil?
-          flash[:danger] = "Cet email n'a pas reçu d'invitation, vous ne pouvez pour l'instant pas créer de compte avec."
-          redirect Users.r(:signup)
-        else
-          begin
-            User.create(request.subset(:email, :password, :age, :laterality, :believer, :sex))
-            if user_login(request.subset(:email, :password))
-              redirect MainController.r(:index)
-            else
-              redirect Users.r(:signup)
-            end
-          rescue => e
-            flash[:danger] = "Erreur lors de la création du compte : #{e.message}"
+        begin
+          User.create(request.subset(:email, :password, :age, :laterality, :believer, :sex))
+          if user_login(request.subset(:email, :password))
+            redirect_referrer
+          else
+            redirect Users.r(:signup)
           end
+        rescue => e
+          flash[:danger] = "Erreur lors de la création du compte : #{e.message}"
         end
       else
         flash[:warning] = "Password et confirmation pas egaux."
