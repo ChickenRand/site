bda1862448b1f8041bcab077657e712c38cde43d
Andréas Livet <andreas.livet@gmail.com>
2017-09-19 12:38:19 +0200

    Questionnaire : warn user before leaving the page

--- public/js/xp/questionnaire.js
+++ public/js/xp/questionnaire.js
@@ -1,5 +1,6 @@
 "use strict";
 $(window).on('questionnaire', function(){
+	var isDirty = true;
 	$('#xp_container').removeClass('outer');
 	$("#results_form").submit(function(e){
 		e.preventDefault();
@@ -8,9 +9,10 @@ $(window).on('questionnaire', function(){
 			var formData = $("#results_form").serializeArray();
 			formData.push({name: 'results', value: JSON.stringify(AVAILABLE_RNG.results)});
 			formData.push({name: 'rng_id', value: AVAILABLE_RNG.id});
-			$('#xp_container').html('Envoi des données en cours, merci de ne pas fermer la page.')
+			$('#xp_container').html('<img src="/images/ajax-loader.gif"></img>Envoi des données en cours, merci de ne pas fermer la page.')
 			$.post("/xp/send_results/" + getXpId(), formData, function(data){
 				$.get('/xp/end_xp', function(html){
+					isDirty = false;
 					$("#xp_container").html(html);
 				});
 			});
@@ -22,4 +24,11 @@ $(window).on('questionnaire', function(){
 			});
 		}
 	});
+
+	window.onbeforeunload = function (e) {
+		if (e.stopPropagation && isDirty) {
+			e.stopPropagation();
+			e.preventDefault();
+		}
+	}
 });
