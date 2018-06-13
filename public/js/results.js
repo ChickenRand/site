f0931fe608fa5e393dc59c7a57de3b138b43f4d2
Genova <aiannucelli@fabdunumerique.fr>
2018-06-13 11:57:59 +0200

    Passage eslint + prettier

--- public/js/results.js
+++ public/js/results.js
@@ -7,11 +7,9 @@ $(() => {
       '<div id="loader"><img src="/images/ajax-loader.gif">  Récupération des données...</div>'
     );
 
-    $.get(`/results/get_results/${window.CURRENT_USER_ID}.json`, function(
-      data
-    ) {
+    $.get(`/results/get_results/${window.CURRENT_USER_ID}.json`, data => {
       if (data.length > 0) {
-        const graph = createCumulativeGraph("#diff_ones_fountain", data);
+        const graph = window.createCumulativeGraph("#diff_ones_fountain", data);
         if (graph.controlPending) {
           $("#diff_ones_fountain").before(
             '<p id="waiting_control">En attente des résultats contrôles...</p>'
